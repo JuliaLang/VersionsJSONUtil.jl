8bb1e36a2f7e75df2d7f7f81f22359711131cd2d
Dilum Aluthge <dilum@aluthge.com>
2026-04-17 00:23:10 -0400

    When writing the JSON file, print dict keys in a deterministic order (by using `OrderedDict`), and customize the sort order

--- src/VersionsJSONUtil.jl
+++ src/VersionsJSONUtil.jl
@@ -2,6 +2,7 @@ module VersionsJSONUtil
 
 using HTTP, JSON, Pkg.BinaryPlatforms, WebCacheUtilities, SHA, Lazy
 import Pkg.BinaryPlatforms: triplet, arch
+using OrderedCollections: OrderedDict
 
 "Wrapper types to define three jlext methods for portable, tarball and installer Windows"
 struct WindowsPortable
@@ -84,7 +85,7 @@ end
 # We're going to collect the combinatorial explosion of version/os-arch possible downloads.
 # We don't have a nice, neat list of what is or is not available, and so we're just going to
 # try and download each file, and if it exists, yay.  Otherwise, bleh.
-julia_platforms = [
+const julia_platforms = [
     # *-linux-gnu
     Linux(:x86_64; libc = :glibc),
     Linux(:i686; libc = :glibc),
@@ -135,8 +136,10 @@ end
 function main(out_path)
     tags = get_tags()
     tag_versions = filter(x -> x !== nothing, [vnum_maybe(basename(t["ref"])) for t in tags])
+    unique!(tag_versions)
+    sort!(tag_versions)
 
-    meta = Dict()
+    meta = OrderedDict()
     number_urls_tried = 0
     number_urls_success = 0
     for version in tag_versions
@@ -171,10 +174,10 @@ function main(out_path)
 
             # Initialize overall version key, if needed
             if !haskey(meta, version)
-                meta[version] = Dict(
-                    "stable" => is_stable(version),
-                    "files" => Vector{Dict}(),
-                )
+                meta[version] = OrderedDict()
+                meta[version]["stable"] = is_stable(version)
+                meta[version]["files"] = Vector{OrderedDict}()
+                sort!(meta[version])
             end
 
             # Test to see if there is an asc signature:
@@ -211,21 +214,23 @@ function main(out_path)
             else
                 error("Unsupported file extension in filename: $(filename)")
             end
-            file_dict = Dict(
-                "triplet" => triplet(platform),
-                "os" => meta_os(platform),
-                "arch" => string(arch(platform)),
-                "version" => string(version),
-                "sha256" => tarball_hash,
-                "size" => filesize(filepath),
-                "kind" => kind,
-                "extension" => extension,
-                "url" => url,
-            )
+            file_dict = OrderedDict()
+            file_dict["triplet"] = triplet(platform)
+            file_dict["os"] = meta_os(platform)
+            file_dict["arch"] = string(arch(platform))
+            file_dict["version"] = string(version)
+            file_dict["sha256"] = tarball_hash
+            file_dict["size"] = filesize(filepath)
+            file_dict["kind"] = kind
+            file_dict["extension"] = extension
+            file_dict["url"] = url
             # Add in `.asc` signature content, if applicable
             if asc_signature !== nothing
                 file_dict["asc"] = asc_signature
             end
+
+            # Explicitly sort the file_dict
+            sort!(file_dict)
 
             # Right now, all we have are archives, but let's be forward-thinking
             # and make this an array of dictionaries that is easy to extensibly match
@@ -233,7 +238,7 @@ function main(out_path)
 
             # Write out new versions of our versions.json as we go
             open(out_path, "w") do io
-                JSON.print(io, meta, 2)
+                JSON.print(io, meta; pretty_print = true, sort_keys = false)
             end
 
             # Delete downloaded file
