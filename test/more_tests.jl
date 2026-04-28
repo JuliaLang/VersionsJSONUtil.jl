b4184afc749e8b025b965ed394ab249a9423a7f7
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-04-28 17:37:00 -0400

    Revert "Add `git-tree-{sha1,sha256}` (but only for `.tar.gz` downloads) (#64)"
    This reverts commit 7fb20f63938f9677513f761d2372c0a968f5151b.

--- test/more_tests.jl
+++ test/more_tests.jl
@@ -67,8 +67,6 @@ end
                     ]
                     optional_keys = [
                         "asc",
-                        "git-tree-sha1",
-                        "git-tree-sha256",
                     ]
                     allowed_keys = union(required_keys, optional_keys)
                     @test required_keys ⊆ collect(keys(filedict))
